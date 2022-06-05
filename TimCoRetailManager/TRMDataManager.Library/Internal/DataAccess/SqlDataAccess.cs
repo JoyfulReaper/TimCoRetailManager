b53775a8ae387d7a9f9d1be25557648634e86b31
Kyle Givler <xvegan88x@gmail.com>
2022-06-05 10:35:31 -0400

    Improve transaction handling

--- TimCoRetailManager/TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TimCoRetailManager/TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -14,7 +14,8 @@ namespace TRMDataManager.Library.Intenal.DataAccess
     {
         private IDbConnection _connection;
         private IDbTransaction _transaction;
-
+        private bool isClosed = false;
+        
         public string GetConnectionString(string name)
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
@@ -66,27 +67,42 @@ namespace TRMDataManager.Library.Intenal.DataAccess
             _connection = new SqlConnection(connectionString);
             _connection.Open();
             _transaction = _connection.BeginTransaction();
+
+            isClosed = false;
         }
 
         public void CommitTransaction()
         {
             _transaction?.Commit();
-            _transaction?.Dispose();
-            _transaction = null;
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void RollbackTransaction()
         {
             _transaction?.Rollback();
-            _transaction?.Dispose();
-            _transaction = null;
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void Dispose()
         {
-            CommitTransaction();
+            if(isClosed == false)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                catch
+                {
+                    // TODO - Log this issue
+                }
+            }
+
+            _transaction = null;
+            _connection = null;
         }
 
         // Open Connection / Start Transaction method
